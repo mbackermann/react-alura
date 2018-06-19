b0d6ccd9976a22b0e086be90e30f4fef217e53ff
Maurício Ackermann <mauricio@agenciaflin.com.br>
2018-06-19 17:11:45 -0300

    Aula 5 - Isolando os componentes do Autor e melhorando a comunicação entre eles

--- src/App.js
+++ src/App.js
@@ -1,124 +1,45 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery';
-import InputCustomizado from './componentes/InputCustomizado.js';
-import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado.js';
+import AutorBox from './Autor'
 
 class App extends Component {
 
-  constructor(){
-    super();
-    this.state = {lista: [], nome: '', email: '', senha: ''};
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  componentDidMount(){
-    $.ajax({
-      url: 'http://localhost:8080/api/autores',
-      dataType: 'json',
-      success: function(data){
-        this.setState({lista: data})
-      }.bind(this)
-    })
-  }
-
-  enviaForm(event){
-    event.preventDefault();
-    $.ajax({
-      url: "http://localhost:8080/api/autores",
-      contentType: "application/json",
-      dataType: "json",
-      type: "post",
-      data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
-      success: function(resposta){
-        this.setState({lista: resposta});
-      }.bind(this),
-      error: function(resposta){
-        console.log("erro!");
-      }
-    })
-  }
-
-  setNome(evento){
-    this.setState({nome: evento.target.value})
-  }
-
-  setEmail(evento){
-    this.setState({email: evento.target.value})
-  }
-
-  setSenha(evento){
-    this.setState({senha: evento.target.value})
-  }
 
   render() {
     return (
       <div id="layout">
 
-    <a href="#menu" id="menuLink" className="menu-link">
+        <a href="#menu" id="menuLink" className="menu-link">
 
-        <span></span>
-    </a>
+          <span></span>
+        </a>
 
-    <div id="menu">
-        <div className="pure-menu">
+        <div id="menu">
+          <div className="pure-menu">
             <a className="pure-menu-heading" href="#">Company</a>
 
             <ul className="pure-menu-list">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
 
 
             </ul>
+          </div>
         </div>
-    </div>
 
         <div id="main">
-            <div className="header">
-              <h1>Cadastro de Autores</h1>
-            </div>
-            <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                  <InputCustomizado id="nome" name="nome" type="text" value={this.state.nome} onChange={this.setNome} label="Nome"/>
-                  <InputCustomizado id="email" name="email" type="email" value={this.state.email} onChange={this.setEmail} label="E-mail"/>
-                  <InputCustomizado id="senha" name="senha" type="password" value={this.state.senha} onChange={this.setSenha} label="Senha"/>
-                  <BotaoSubmitCustomizado label="Gravar" />
-                </form>
-
-              </div>
-              <div>
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  {
-                    this.state.lista.map(function(author){
-                      return (
-                        <tr key={author.id}>
-                          <td>{author.nome}</td>
-                          <td>{author.email}</td>
-                        </tr>
-                      );
-                    })
-                  }
-                  </tbody>
-                </table>
-              </div>
-            </div>
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
           </div>
+          <div className="content" id="content">
+            <AutorBox/>
+          </div>
+        </div>
 
 
-</div>
+      </div>
     );
   }
 }
